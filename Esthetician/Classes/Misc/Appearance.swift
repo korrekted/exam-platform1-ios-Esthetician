e42aee697609dcddf15a151af055b4b339ab935e
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-07-21 12:25:54 +0300

    copy

---
+++ Esthetician/Classes/Misc/Appearance.swift
@@ -0,0 +1,21 @@
+//
+//  Appearance.swift
+//  Ged
+//
+//  Created by Andrey Chernyshev on 23.04.2021.
+//
+
+import UIKit
+
+struct Appearance {
+    static let backgroundColor = UIColor(integralRed: 255, green: 254, blue: 247)
+    
+    static let mainColor = UIColor(integralRed: 255, green: 124, blue: 59)
+    static let mainColorAlpha = UIColor(integralRed: 255, green: 124, blue: 59, alpha: 0.3)
+    
+    static let takeFreeTestColor = UIColor(integralRed: 254, green: 101, blue: 59)
+    
+    static let progress1Color = UIColor(integralRed: 255, green: 124, blue: 59)
+    static let progress2Color = UIColor(integralRed: 254, green: 101, blue: 59)
+    static let progress3Color = UIColor(integralRed: 56, green: 184, blue: 255)
+}
